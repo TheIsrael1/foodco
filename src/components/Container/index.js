9956cff949ad51fd88b4483fe6a1bd10e423fda5
TheIsrael1 <iehindero419@stu.ui.edu.ng>
2022-01-04 12:26:32 +0200

    'folderSetup'

---
+++ src/components/Container/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './container.module.css'
+
+const Container = () => {
+    return (
+        <div>
+            here
+        </div>
+    )
+}
+
+export default Container
